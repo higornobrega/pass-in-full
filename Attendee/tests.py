5b008d63263cfd7c36a16c5870673db02c369ecc
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-23 21:03:51 -0300

    Test for ateendee list url

--- Attendee/tests.py
+++ Attendee/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
 
 # Create your tests here.
 class AtetendeeURLTest(TestCase):
-    def test_the_pytest_is_ok(self):
-        ...
+    def test_list_attendee_url_is_correct(self):
+        reverse_url = reverse('attendee:list_attendee')
+        print('reverse_url')
+        print(reverse_url)
+        self.assertEqual(reverse_url, '/attendee/list_attendee/')
