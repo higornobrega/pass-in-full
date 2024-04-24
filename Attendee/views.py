5a4a4f59c6ca6c2b7640e3465b3646f4468dc8e5
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-23 21:34:03 -0300

    Created def to create attendee

--- Attendee/views.py
+++ Attendee/views.py
@@ -2,5 +2,8 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def list_ateendee(request: HttpRequest) -> HttpResponse:
+def list_attendee(request: HttpRequest) -> HttpResponse:
     return render(request, 'attendee/list_attendee.html') 
+
+def create_attendee(request: HttpRequest) -> HttpResponse:
+    return render(request, 'attendee/create_attendee.html')
