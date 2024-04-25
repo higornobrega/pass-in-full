d9e08e2b66330dcf7874e3320f7e405f082723bb
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-25 19:26:11 -0300

    Refactoration - Splitting the attendee tests into 2 files for better organization

---
+++ Attendee/tests/test_attendee_url.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
+# Create your tests here.
+class AttendeeURLTest(TestCase):
+    def test_create_attendee_url_is_correct(self):
+        url = reverse('attendee:create_attendee')
+        self.assertEqual(url, '/attendee/create_attendee/')
+    
+    def test_list_attendee_url_is_correct(self):
+        url = reverse('attendee:list_attendee')
+        self.assertEqual(url, '/attendee/list_attendee/')    
+        
+    def test_update_attendee_url_is_correct(self):
+        url = reverse('attendee:update_attendee', args=[1]) # kwargs={'pk': 1}
+        self.assertEqual(url, '/attendee/update_attendee/1/')
+        
+    def test_delete_attendee_url_is_correct(self):
+        url = reverse('attendee:delete_attendee', args=[1]) # kwargs={'pk': 1}
+        self.assertEqual(url, '/attendee/delete_attendee/1/')
+        
--- Attendee/tests/test_attendee_view.py
+++ Attendee/tests/test_attendee_view.py
@@ -4,25 +4,6 @@ from django.urls import resolve, reverse
 from Attendee import views
 
 
-# Create your tests here.
-class AtetendeeURLTest(TestCase):
-    def test_create_attendee_url_is_correct(self):
-        url = reverse('attendee:create_attendee')
-        self.assertEqual(url, '/attendee/create_attendee/')
-    
-    def test_list_attendee_url_is_correct(self):
-        url = reverse('attendee:list_attendee')
-        self.assertEqual(url, '/attendee/list_attendee/')    
-        
-    def test_update_attendee_url_is_correct(self):
-        url = reverse('attendee:update_attendee', args=[1]) # kwargs={'pk': 1}
-        self.assertEqual(url, '/attendee/update_attendee/1/')
-        
-    def test_delete_attendee_url_is_correct(self):
-        url = reverse('attendee:delete_attendee', args=[1]) # kwargs={'pk': 1}
-        self.assertEqual(url, '/attendee/delete_attendee/1/')
-        
-        
 class AttendeeViewTest(TestCase):
     def test_attendee_create_attendee_view_function_is_correct(self):
         view = resolve(reverse('attendee:create_attendee'))
