d4f187bac270235d17fa2d48e3082b3feb5bcca4
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-25 19:55:46 -0300

    Test - Tests to veriify that attendee vies are retuning  to the right template

--- Attendee/tests/test_attendee_view.py
+++ Attendee/tests/test_attendee_view.py
@@ -36,3 +36,21 @@ class AttendeeViewTest(TestCase):
     def test_attendee_delete_attendee_view_returns_status_code_200(self):
         response = self.client.get(reverse('attendee:delete_attendee', kwargs={'pk': 1}))
         self.assertEqual(response.status_code, 200)
+        
+    def test_attendee_create_attendee_view_loads_correct_template(self):
+        response = self.client.get(reverse('attendee:create_attendee'))
+        self.assertTemplateUsed(response, 'attendee/create_attendee.html')
+        
+    def test_attendee_list_attendee_view_loads_correct_template(self):
+        response = self.client.get(reverse('attendee:list_attendee'))
+        self.assertTemplateUsed(response, 'attendee/list_attendee.html')
+        
+    def test_attendee_update_attendee_view_loads_correct_template(self):
+        response = self.client.get(reverse('attendee:update_attendee', kwargs={'pk': 1}))
+        self.assertTemplateUsed(response, 'attendee/update_attendee.html')
+    
+    def test_attendee_delete_attendee_view_loads_correct_template(self):
+        response = self.client.get(reverse('attendee:delete_attendee', kwargs={'pk': 1}))
+        self.assertTemplateUsed(response,'attendee/list_attendee.html' )
+    
+    
