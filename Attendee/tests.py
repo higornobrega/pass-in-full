099071f71f7751e74a0b85fbc03799c033bd64d9
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-24 22:16:57 -0300

    Refactoring - Refactored the function names of the   AttendeeViewTest class

--- Attendee/tests.py
+++ Attendee/tests.py
@@ -24,18 +24,18 @@ class AtetendeeURLTest(TestCase):
         
         
 class AttendeeViewTest(TestCase):
-    def test_attendee_create_attendee_is_correct(self):
+    def test_attendee_create_attendee_view_function_is_correct(self):
         view = resolve(reverse('attendee:create_attendee'))
         self.assertIs(view.func, views.create_attendee)
     
-    def test_attendee_list_attendee_is_correct(self):
+    def test_attendee_list_attendee_view_function_is_correct(self):
         view = resolve(reverse('attendee:list_attendee'))
         self.assertIs(view.func, views.list_attendee)
     
-    def test_attendee_update_attendee_is_correct(self):
+    def test_attendee_update_attendee_view_function_is_correct(self):
         view = resolve(reverse('attendee:update_attendee', kwargs={'pk': 1}))
         self.assertIs(view.func, views.update_attendee)
     
-    def test_attendee_delete_attendee_is_correct(self):
+    def test_attendee_delete_attendee_view_function_is_correct(self):
         view = resolve(reverse('attendee:delete_attendee', kwargs={'pk': 1}))
         self.assertIs(view.func, views.delete_attendee)
