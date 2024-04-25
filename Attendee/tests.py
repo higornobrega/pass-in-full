abebc09a9da413fcc1e9a963e5f009099d23ed06
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-24 21:33:35 -0300

    Refactoring - Changed the variable name reverse_url to url

--- Attendee/tests.py
+++ Attendee/tests.py
@@ -5,17 +5,17 @@ from django.urls import reverse
 # Create your tests here.
 class AtetendeeURLTest(TestCase):
     def test_list_attendee_url_is_correct(self):
-        reverse_url = reverse('attendee:list_attendee')
-        self.assertEqual(reverse_url, '/attendee/list_attendee/')
+        url = reverse('attendee:list_attendee')
+        self.assertEqual(url, '/attendee/list_attendee/')
         
     def test_create_attendee_url_is_correct(self):
-        reverse_url = reverse('attendee:create_attendee')
-        self.assertEqual(reverse_url, '/attendee/create_attendee/')
+        url = reverse('attendee:create_attendee')
+        self.assertEqual(url, '/attendee/create_attendee/')
         
     def test_update_attendee_url_is_correct(self):
-        reverse_url = reverse('attendee:update_attendee', args=[1])
-        self.assertEqual(reverse_url, '/attendee/update_attendee/1/')
+        url = reverse('attendee:update_attendee', args=[1])
+        self.assertEqual(url, '/attendee/update_attendee/1/')
         
     def test_delete_attendee_url_is_correct(self):
-        reverse_url = reverse('attendee:delete_attendee', args=[1])
-        self.assertEqual(reverse_url, '/attendee/delete_attendee/1/')
+        url = reverse('attendee:delete_attendee', args=[1])
+        self.assertEqual(url, '/attendee/delete_attendee/1/')
