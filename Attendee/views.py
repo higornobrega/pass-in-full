d19c2c217c5504b29cc5b818952f7bb3f52985c9
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-24 21:58:06 -0300

    Test - Tenting whether the  create_attendee route  is leading to the create_attendee funtion

--- Attendee/views.py
+++ Attendee/views.py
@@ -2,11 +2,11 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def list_attendee(request: HttpRequest) -> HttpResponse:
-    return render(request, 'attendee/list_attendee.html') 
-
 def create_attendee(request: HttpRequest) -> HttpResponse:
     return render(request, 'attendee/create_attendee.html')
+
+def list_attendee(request: HttpRequest) -> HttpResponse:
+    return render(request, 'attendee/list_attendee.html') 
 
 def update_attendee(request: HttpRequest, pk: int) -> HttpResponse:
     return render(request, 'attendee/update_attendee.html')
