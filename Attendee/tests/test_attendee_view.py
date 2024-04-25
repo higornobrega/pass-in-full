3a8e8b59b13106055df1701e0ac4d32ebef8f922
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-25 19:44:44 -0300

    Test - Tests for to ensure that attendee views are returning status code 200

--- Attendee/tests/test_attendee_view.py
+++ Attendee/tests/test_attendee_view.py
@@ -20,3 +20,19 @@ class AttendeeViewTest(TestCase):
     def test_attendee_delete_attendee_view_function_is_correct(self):
         view = resolve(reverse('attendee:delete_attendee', kwargs={'pk': 1}))
         self.assertIs(view.func, views.delete_attendee)
+        
+    def test_attendee_create_attendee_view_returns_code_200(self):
+        response = self.client.get(reverse('attendee:create_attendee'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_attendee_list_attendee_view_returns_status_code_200(self):
+        response = self.client.get(reverse('attendee:list_attendee'))
+        self.assertEqual(response.status_code,200)
+        
+    def test_attendee_update_attendee_view_returns_status_code_200(self):
+        response = self.client.get(reverse('attendee:update_attendee', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_attendee_delete_attendee_view_returns_status_code_200(self):
+        response = self.client.get(reverse('attendee:delete_attendee', kwargs={'pk': 1}))
+        self.assertEqual(response.status_code, 200)
