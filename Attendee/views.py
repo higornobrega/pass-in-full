4150e218a9981601850c735f1df14ce4b2fce0f4
Higor Nóbrega <higorst.nobrega@gmail.com>
2024-04-26 21:52:58 -0300

    Feature - List all Ateendees and send to the fornt

--- Attendee/views.py
+++ Attendee/views.py
@@ -1,12 +1,18 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Attendee
+
 
 def create_attendee(request: HttpRequest) -> HttpResponse:
     return render(request, 'attendee/create_attendee.html')
 
 def list_attendee(request: HttpRequest) -> HttpResponse:
-    return render(request, 'attendee/list_attendee.html') 
+    attendees = Attendee.objects.all().order_by('name')
+    context = {
+        'attendees':attendees,
+    }
+    return render(request, 'attendee/list_attendee.html', context) 
 
 def update_attendee(request: HttpRequest, pk: int) -> HttpResponse:
     return render(request, 'attendee/update_attendee.html')
